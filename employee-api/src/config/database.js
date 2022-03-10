058c3981f6bc41e66370b269dd92fe63cd678988
Francisco Guilherme <fguilhermedev@gmail.com>
2022-03-10 12:59:44 -0300

    "fix bug"

---
+++ employee-api/src/config/database.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
+pool.on('error', () => {
+  console.log('Unexpected error occurred')
+  process.exit(-1);
+});
+
+pool.on('connect', () => {
+  console.log('Base de Dados conectada com sucesso!')
+});
+
+module.exports = {
+  query(text, params) => pool.query(text, params),
+};
+
